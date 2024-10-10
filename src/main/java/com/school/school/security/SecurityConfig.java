ed89173d82a86c7883d592e1d372591eae0449e4
Chris <chrizgx@users.noreply.github.com>
2024-10-10 18:25:47 +0300

    Remove authentication provider code

--- src/main/java/com/school/school/security/SecurityConfig.java
+++ src/main/java/com/school/school/security/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.school.school.security;
 
-
-// import com.javacodeex.filter.JwtRequestFilter;
-// import com.javacodeex.service.CustomUserDetailsService;
 import com.school.school.service.CustomUserDetailsService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,27 +13,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
 @Slf4j
-@EnableMethodSecurity(
-    prePostEnabled = true,
-    securedEnabled = true,
-    jsr250Enabled = true
-)
 public class SecurityConfig {
 
     private final JwtRequestFilter jwtRequestFilter;
-    private final JwtAuthenticationProvider jwtAuthenticationProvider;
     private final CustomUserDetailsService customUserDetailsService;
 
-    public SecurityConfig(JwtRequestFilter jwtRequestFilter, JwtAuthenticationProvider jwtAuthenticationProvider, CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter, CustomUserDetailsService customUserDetailsService) {
         this.jwtRequestFilter = jwtRequestFilter;
-        this.jwtAuthenticationProvider = jwtAuthenticationProvider;
         this.customUserDetailsService = customUserDetailsService;
     }
 
@@ -57,30 +44,10 @@ public class SecurityConfig {
         return http.build();
     }
 
-        @Autowired
-        public void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.authenticationProvider(jwtAuthenticationProvider);
-        }
-
-        @Bean
-        public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
-            return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                    .authenticationProvider(jwtAuthenticationProvider)
-                    .build();
-        }
-
-
-    // @Bean
-    // public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    //     return authenticationConfiguration.getAuthenticationManager();
-    // }
-
-    // @Bean
-    // @Lazy
-    // public AuthenticationManager authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-    //     authenticationManagerBuilder.authenticationProvider(this.jwtAuthenticationProvider);
-    //     return authenticationManagerBuilder.build();
-    // }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
