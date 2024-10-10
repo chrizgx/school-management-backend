52cd2cba7e6186098e134bf35f1e4194a402ac84
Chris <chrizgx@users.noreply.github.com>
2024-10-10 10:45:42 +0300

    Fix import issue

--- src/main/java/com/school/school/entity/Department.java
+++ src/main/java/com/school/school/entity/Department.java
@@ -3,6 +3,7 @@ package com.school.school.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -18,5 +19,5 @@ public class Department {
 
     @Column(name = "DESCRIPTION")
     private String description;
-    
+
 }
