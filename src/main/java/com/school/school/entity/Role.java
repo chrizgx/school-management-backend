3cbd5f4355f104a1d48f43108cfb89fc9f4ac98a
Chris <chrizgx@users.noreply.github.com>
2024-10-12 03:03:08 +0300

    Refactor for best practices

--- src/main/java/com/school/school/entity/Role.java
+++ src/main/java/com/school/school/entity/Role.java
@@ -1,5 +1,18 @@
 package com.school.school.entity;
 
 public enum Role {
-    ADMIN, HELP_DESK, TEACHER, STUDENT
+    ADMIN("ADMIN"),
+    HELP_DESK("HELP_DESK"),
+    TEACHER("TEACHER"),
+    STUDENT("STUDENT");
+
+    private final String displayName;
+
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String toString() {
+        return displayName;
+    }
 }
