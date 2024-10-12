a1fc5799cb23b55e3d499acdebeaea75fbececa8
Chris <chrizgx@users.noreply.github.com>
2024-10-12 19:07:47 +0300

    Create PUT:api/user/{id} path and functionality available only to admin and help_desk

--- src/main/java/com/school/school/controller/UserController.java
+++ src/main/java/com/school/school/controller/UserController.java
@@ -5,6 +5,8 @@ import com.school.school.entity.Role;
 import com.school.school.service.CustomUserDetailsService;
 import com.school.school.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.access.prepost.PreAuthorize; 
 
 // import jakarta.annotation.security.RolesAllowed;
@@ -15,17 +17,23 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Autowired
     private UserService userService;
 
     @GetMapping
@@ -43,6 +51,26 @@ public class UserController {
             return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('HELP_DESK')")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Integer id, @RequestBody User user, @RequestAttribute("id") Integer requestorId, @RequestAttribute("role") Role requestorRole) {
+        try {
+            log.info("PUT:/api/user/" + id + "(-)");
+            // Role requestorRole = Role.getFromString(requestorRoleString);
+            User updatedUser = userService.updateUserGuard(id, user, requestorId, requestorRole);
+
+            if (updatedUser != null) {
+                return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            log.info("Error: " + e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
--- src/main/java/com/school/school/service/UserService.java
+++ src/main/java/com/school/school/service/UserService.java
@@ -52,4 +52,41 @@ public class UserService {
 
         return null;
     }
+
+    // 
+    public User updateUserGuard(Integer id, User user, Integer requestorId, Role requestorRole) {
+        Optional<User> existingUser = userRepository.findById(id);
+
+        if (!existingUser.isPresent()) return null;
+        User userToUpdate = existingUser.get(); 
+
+        // Authentication Logic starts here
+        // Admin can edit everyone's profile and ALL attributes
+        if (requestorRole == Role.ADMIN) return updateUser(userToUpdate.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getRole(), user.isEnabled());
+
+        // Help desk can edit all role's profile except Admin's and Help Desks (including themselves)
+        // Help desk CANNOT edit user role.
+        if (requestorRole == Role.HELP_DESK && userToUpdate.getRole() != Role.ADMIN && userToUpdate.getRole() != Role.HELP_DESK)
+            return updateUser(userToUpdate.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), null, user.isEnabled());
+
+        log.info("updateUserGuard(- ID# " + requestorId + " illegally tried to update profile ID#" + user.getId());
+        return null;
+
+    }
+
+    public User updateUser(Long id, String firstName, String lastName, String email, Role role, Boolean enabled) {
+        Optional<User> existingUser = userRepository.findById(id);
+
+        if (!existingUser.isPresent()) return null;
+
+        User user = existingUser.get();
+
+        if (firstName != null) user.setFirstName(firstName);
+        if (lastName != null) user.setLastName(lastName);
+        if (email != null) user.setEmail(email);
+        if (role != null) user.setRole(role);
+        if (enabled != null) user.setEnabled(enabled);
+
+        return userRepository.save(user);
+    }
 }
