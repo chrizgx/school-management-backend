08b175b4ee1ee5cbd08eaa6c95ffe259fd0417d1
Chris <chrizgx@users.noreply.github.com>
2024-10-12 18:27:08 +0300

    Optimize current paths

--- src/main/java/com/school/school/controller/UserController.java
+++ src/main/java/com/school/school/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.school.school.controller;
 
 import com.school.school.entity.User;
+import com.school.school.entity.Role;
 import com.school.school.service.CustomUserDetailsService;
+import com.school.school.service.UserService;
+
 import org.springframework.security.access.prepost.PreAuthorize; 
 
 // import jakarta.annotation.security.RolesAllowed;
@@ -22,19 +25,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     @Autowired
-    private CustomUserDetailsService userService;
+    private CustomUserDetailsService userDetailsService;
+    private UserService userService;
 
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN') hasRole('HELP_DESK')")
-    public Iterable<User> getMethodName() {
+    @PreAuthorize("hasRole('ADMIN') or hasRole('HELP_DESK')")
+    public Iterable<User> getAllUsers() {
 
-        return userService.getAllUsers();
+        return userDetailsService.getAllUsers();
     }
     
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('HELP_DESK')")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         try {
-            User createdUser = userService.createUser(user);
+            User createdUser = userDetailsService.createUser(user);
             return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
