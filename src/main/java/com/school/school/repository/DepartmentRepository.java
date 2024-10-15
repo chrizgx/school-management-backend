ce3edfcf118db946ba94473b4c4e924221d3c489
Chris <chrizgx@users.noreply.github.com>
2024-10-15 15:55:59 +0300

    Create method to get departments WITHOUT enrolled users id (privacy)

--- src/main/java/com/school/school/repository/DepartmentRepository.java
+++ src/main/java/com/school/school/repository/DepartmentRepository.java
@@ -1,6 +1,7 @@
 package com.school.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.school.school.entity.Department;
 
@@ -9,5 +10,8 @@ import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
     Optional<Department> findById(Integer id);
+
+    @Query("select d from Department d")
+    List<Department> findAllWithoutFindingUsers();
 
 }
