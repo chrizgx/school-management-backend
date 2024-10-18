51fc2e7f846f277b28d13db755edb74e818e3e1a
Chris <chrizgx@users.noreply.github.com>
2024-10-18 12:40:48 +0300

    Implement standard ResponseWrapper for all controller requests

--- src/main/java/com/school/school/controller/DepartmentsController.java
+++ src/main/java/com/school/school/controller/DepartmentsController.java
@@ -4,6 +4,7 @@ import com.school.school.entity.User;
 import com.school.school.entity.Role;
 import com.school.school.entity.Department;
 import com.school.school.repository.DepartmentRepository;
+import com.school.school.response.ResponseWrapper;
 import com.school.school.service.DepartmentService;
 import com.school.school.exception.UserAlreadyEnrolledException;
 
@@ -38,51 +39,50 @@ public class DepartmentsController {
     private DepartmentRepository departmentRepository;
 
     @GetMapping
-    public ResponseEntity<Iterable<Department>> getDepartments() {
+    public ResponseEntity<ResponseWrapper<Iterable<Department>>> getDepartments() {
         Iterable<Department> departments = departmentRepository.findAll();
-        return new ResponseEntity<>(departments, HttpStatus.OK);
+        return ResponseEntity.ok(new ResponseWrapper<>(departments, true));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
+    public ResponseEntity<ResponseWrapper<Department>> getDepartmentById(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
         Department department = departmentService.findById(id, requestorRole);
         if (department == null) return ResponseEntity.notFound().build();
-
-        return new ResponseEntity<>(department, HttpStatus.OK);
+        return ResponseEntity.ok(new ResponseWrapper<>(department, true));
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Department> createDepartment(@RequestBody Department newDepartment, @RequestAttribute("role") Role requestorRole) {
+    public ResponseEntity<ResponseWrapper<Department>> createDepartment(@RequestBody Department newDepartment, @RequestAttribute("role") Role requestorRole) {
         Department department = departmentService.create(newDepartment);
 
         if (department == null) return ResponseEntity.badRequest().build();
-        return new ResponseEntity<>(department, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper<>(department, true));
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Department> updateDepartment(@PathVariable Integer id, @RequestBody Department updatedDepartment) {
+    public ResponseEntity<ResponseWrapper<Department>> updateDepartment(@PathVariable Integer id, @RequestBody Department updatedDepartment) {
         Department department = departmentService.update(id, updatedDepartment);
         
         if (department == null) return ResponseEntity.notFound().build();
-        return new ResponseEntity<>(department, HttpStatus.ACCEPTED);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper<>(department, true));
     }
 
     @PutMapping("/{id}/enable")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Department> enable(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
+    public ResponseEntity<ResponseWrapper<Department>> enable(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
         return updateDepartmentStatus(id, true, requestorRole);
     }
 
     @PutMapping("/{id}/disable")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Department> disable(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
+    public ResponseEntity<ResponseWrapper<Department>> disable(@PathVariable("id") Integer id, @RequestAttribute("role") Role requestorRole) {
         return updateDepartmentStatus(id, false, requestorRole);
     }
 
 
-    private ResponseEntity<Department> updateDepartmentStatus(Integer id, boolean enable, Role requestorRole) {
+    private ResponseEntity<ResponseWrapper<Department>> updateDepartmentStatus(Integer id, boolean enable, Role requestorRole) {
         Department department;
         Boolean updated;
         
@@ -91,20 +91,20 @@ public class DepartmentsController {
         if (updated == false) return ResponseEntity.badRequest().build();
         
         department = departmentService.findById(id, requestorRole);
-        return new ResponseEntity<>(department, HttpStatus.ACCEPTED);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper<>(department, true));
     }
 
     @PostMapping("/{id}/student/{userId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('HELP_DESK')")
-    public ResponseEntity<String> enrollStudent(@PathVariable("id") Integer departmentId, @PathVariable("userId") Integer userId) {
+    public ResponseEntity<ResponseWrapper<Void>> enrollStudent(@PathVariable("id") Integer departmentId, @PathVariable("userId") Integer userId) {
         try {
             Boolean enroll = departmentService.enrollStudent(departmentId, userId);
-            if (enroll) return ResponseEntity.ok().build();
+            if (enroll) return ResponseEntity.ok(new ResponseWrapper<>("User ID#" + userId + " successfully enrolled to Department ID#" + departmentId, true));
             
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseWrapper<>("User is either not a student or does not exist.", false))
         } catch (UserAlreadyEnrolledException e) {
             log.info("Error: " + e);
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseWrapper<>("User ID#" + userId + " is already enrolled to Department ID#" + departmentId, false));
         }
     }
 
