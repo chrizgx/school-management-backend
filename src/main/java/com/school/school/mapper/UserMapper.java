70afa5d5766edc7679a41c611fd10764edda1359
Chris <chrizgx@users.noreply.github.com>
2024-10-24 17:55:16 +0300

    Create map to translate CreateUserDTO to User entity

--- src/main/java/com/school/school/mapper/UserMapper.java
+++ src/main/java/com/school/school/mapper/UserMapper.java
@@ -11,4 +11,8 @@ public class UserMapper {
     public UserDTO toUserDTO(User user) {
         return new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getRole(), user.isEnabled());
     }
+
+    public User toUser(CreateUserDTO user) {
+        return new User(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword(), user.getRole(), user.isEnabled());
+    }
 }
